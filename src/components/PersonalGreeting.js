f3b567a4fe360a24ccf19c1e66df5e3bac008e0d
danish0501 <alidanish373@gmail.com>
2023-07-26 16:29:27 +0530

    submitting code-Wed Jul 26 2023 16:29:25 GMT+0530 (India Standard Time)

--- src/components/PersonalGreeting.js
+++ src/components/PersonalGreeting.js
@@ -1,30 +1,52 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-const PersonalGreeting= () => {
-  const [name, setName] = useState('');
-  const [greeting, setGreeting] = useState('');
+// const PersonalGreeting= () => {
+//   const [name, setName] = useState('');
+//   const [greeting, setGreeting] = useState('');
 
-  const handleChange = (event) => {
-    setName(event.target.value);
-  };
+//   const handleChange = (event) => {
+//     setName(event.target.value);
+//   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setGreeting(`Hello, ${name}!`);
-  };
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     setGreeting(`Hello, ${name}!`);
+//   };
 
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         <label>
+//           Enter your name:
+//           <input type="text" value={name} onChange={handleChange} />
+//         </label>
+//         <button type="submit">Submit</button>
+//       </form>
+//       {greeting && <p>{greeting}</p>}
+//     </div>
+//   );
+// };
+
+// export default PersonalGreeting;
+
+
+
+import React,{useState} from "react";
+
+const App = () => {
+  let [inputtext, setinputtext]=useState();
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Enter your name:
-          <input type="text" value={name} onChange={handleChange} />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-      {greeting && <p>{greeting}</p>}
-    </div>
-  );
-};
+        {/* Do not remove the main div */}
+        <p>Enter Your name:</p>
+        <input type="text" onChange={(e)=>(setinputtext(e.target.value))}/>
 
-export default PersonalGreeting;
+
+        {
+          inputtext && <p>Hello {inputtext}!</p>
+        }
+    </div>
+  )
+}
+
+export default App
